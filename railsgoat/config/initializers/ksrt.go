ed934e4daf406e165d4b1b11439e5e4b128d7857
Kseniia Smirnova <115711811+KseniiaSmirn0va@users.noreply.github.com>
2024-02-01 17:17:20 +0300

    Update ksrt.go

--- railsgoat/config/initializers/ksrt.go
+++ railsgoat/config/initializers/ksrt.go
@@ -1,24 +1,6 @@
 DetectorType_JiraToken                     DetectorType = 120
 Token 23417846781247eef
+Redacted:     "AKIASP2TPHJSQH3FJRUX
+"account":        "171436882533"
+"user_id":        "AIDASP2TPHJSUFRSTTZX4"
 
-
-			want: []detectors.Result{
-				{
-					DetectorType: detectorspb.DetectorType_AWS,
-					Verified:     true,
-					Redacted:     "AKIASP2TPHJSQH3FJRUX",
-					ExtraData: map[string]string{
-3 months ago
-
-added resource type mapping to extraData in AWS (#2087)
-						"resource_type":  "Access key",
-						"rotation_guide": "https://howtorotate.com/docs/tutorials/aws/",
-						"account":        "171436882533",
-						"arn":            "arn:aws:iam::171436882533:user/canarytokens.com@@4dxkh0pdeop3bzu9zx5wob793",
-						"user_id":        "AIDASP2TPHJSUFRSTTZX4",
-5 months ago
-
-Retry AWS verification 403s (#1757)
-					},
-				},
-			},
