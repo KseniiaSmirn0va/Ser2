ca1ca5cac13efd2e969570d2aae78ecd527b2dbb
Kseniia Smirnova <115711811+KseniiaSmirn0va@users.noreply.github.com>
2024-02-02 09:49:00 +0300

    Update ksrt.go

--- railsgoat/config/initializers/ksrt.go
+++ railsgoat/config/initializers/ksrt.go
@@ -1,8 +1,5 @@
 DetectorType_JiraToken                     DetectorType = 120
 Token 23417846781247eef
 AKIASP2TPHJSQH3FJRUX
-"account":        "171436882533"
-"user_id":        "AIDASP2TPHJSUFRSTTZX4"
-`DENO_KV_ACCESS_TOKEN=rgesves456c`,
-`etrTOKEN=`ddp_hn029Cl2dIN4Jb0BF0L1V9opokoPVC30ddGk`,`,
-TOKEN	: 'cdf_hn029Cl2dIN4Jb0BF0L1V9opokoPVC30ddGk'		
+
+data:   []byte(fmt.Sprintf("25:KEY='rzp_live_SnDTaP1ncfliDt'\n\"rzp_secret\" : \" %s\", ", secretInactive)),
