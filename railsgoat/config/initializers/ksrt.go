34059a60ab9065a6caf563faf6efe1cd7bb766eb
Kseniia Smirnova <115711811+KseniiaSmirn0va@users.noreply.github.com>
2024-02-02 08:06:38 +0300

    Update ksrt.go

--- railsgoat/config/initializers/ksrt.go
+++ railsgoat/config/initializers/ksrt.go
@@ -1,6 +1,7 @@
 DetectorType_JiraToken                     DetectorType = 120
 Token 23417846781247eef
-Redacted:     "AKIASP2TPHJSQH3FJRUX
+AKIASP2TPHJSQH3FJRUX
 "account":        "171436882533"
 "user_id":        "AIDASP2TPHJSUFRSTTZX4"
-
+`DENO_KV_ACCESS_TOKEN=ddp_hn029Cl2dIN4Jb0BF0L1V9opokoPVC30ddGk`,
+			
